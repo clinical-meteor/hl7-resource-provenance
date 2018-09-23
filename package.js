44bb0566ecd85b50c82daa0d78c6b0312c5892fc
Abigail Watson <abigailwatson@Abigails-Mac-mini-380428.local>
2018-09-23 14:29:57 -0500

    deps update

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-provenance',
-  version: '1.1.2',
+  version: '1.1.3',
   summary: 'HL7 FHIR Resource - Provenance',
   git: 'https://github.com/clinical-meteor/hl7-resource-provenance',
   documentation: 'README.md'
@@ -11,14 +11,24 @@ Package.onUse(function (api) {
 
   api.use('meteor-platform');
   api.use('mongo');
-  api.use('aldeed:simple-schema@1.3.3');
-  api.use('aldeed:collection2@2.5.0');
   api.use('simple:json-routes@2.1.0');
-  api.use('prime8consulting:meteor-oauth2-server@0.0.2');
+  api.use('momentjs:moment@2.17.1');
+  api.use('ecmascript@0.9.0');
+  api.use('session');
+  api.use('http');
+  api.use('react-meteor-data@0.2.15');
 
-  api.use('clinical:base-model@1.3.5');
-  api.use('clinical:hl7-resource-datatypes@0.6.0');
+  api.use('clinical:glass-ui@2.2.1');
+  api.use('clinical:extended-api@2.2.2');
+  api.use('clinical:base-model@1.4.0');
+  api.use('clinical:user-model@1.6.2');
+  api.use('matb33:collection-hooks@0.7.15');
+  api.use('clinical:alert@2.1.3');
 
+  if(Package['clinical:fhir-vault-server']){
+    api.use('clinical:fhir-vault-server@0.0.3', ['client', 'server'], {weak: true});
+  }
+  
   api.addFiles('lib/hl7-resource-provenance.js', ['client', 'server']);
   api.addFiles('server/rest.js', 'server');
   api.addFiles('server/initialize.js', 'server');
@@ -27,3 +37,10 @@ Package.onUse(function (api) {
   api.export('Provenances');
   api.export('ProvenanceSchema');
 });
+
+Npm.depends({
+  "simpl-schema": "1.5.3",
+  "moment": "2.22.2",
+  "lodash": "4.17.4"
+})
+
